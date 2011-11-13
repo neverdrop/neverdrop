79fd19d600816c8e460210c29a06580f2fb90df9
Vladimir Kostyukov <vladimir.kostukov@gmail.com>
2011-11-13 20:23:54 +0700

    fix b2d

--- js/scenes.js
+++ js/scenes.js
@@ -21,18 +21,6 @@ var utils = require("utils");
 
 var indexer = 0;
 
-b2AABB = Box2D.Collision.b2AABB;
-b2World = Box2D.Dynamics.b2World;
-b2Vec2 = Box2D.Common.Math.b2Vec2;
-b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
-b2Body = Box2D.Dynamics.b2Body;
-b2BodyDef = Box2D.Dynamics.b2BodyDef;
-b2FixtureDef = Box2D.Dynamics.b2FixtureDef;
-b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape;
-b2CircleShape = Box2D.Collision.Shapes.b2CircleShape;
-b2MouseJointDef = Box2D.Dynamics.Joints.b2MouseJointDef;
-b2ContactListener = Box2D.Dynamics.b2ContactListener;
-
 var AbstractScene = function() {
 
 	this.models = {};
